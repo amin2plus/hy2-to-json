2ebc4d8426be94282b3becf97eecfa6a4dba0829
Amin <31206022+amin2plus@users.noreply.github.com>
2024-01-20 09:22:36 +0330

    Update hy2-to-json.py

--- hy2-to-json.py
+++ hy2-to-json.py
@@ -23,124 +23,165 @@ def generate_json(input_link):
 
     # Construct the JSON structure
     output_json = {
-        "dns": {
-            "independent_cache": True,
-            "rules": [
-                {
-                    "disable_cache": True,
-                    "geosite": ["category-ads-all"],
-                    "server": "dns-block"
-                }
-            ],
-            "servers": [
-                {
-                    "address": "tcp://94.140.14.14",
-                    "address_resolver": "dns-direct",
-                    "strategy": "ipv4_only",
-                    "tag": "dns-remote"
+       "outbounds": [
+           {
+               "type": "selector",
+               "tag": "Select",
+               "outbounds": [
+                   "Auto",
+                   "hy2"
+               ],
+               "default": "Auto"
+           },
+           {
+               "type": "urltest",
+               "tag": "Auto",
+               "outbounds": [
+                   "hy2"
+               ],
+               "url": "https://www.gstatic.com/generate_204",
+               "interval": "10m",
+               "tolerance": 200
+           },
+           {
+               "tag": "direct",
+               "type": "direct"
+           },
+           {
+               "tag": "block",
+               "type": "block"
+           },
+           {
+               "tag": "dns-out",
+               "type": "dns"
+           },
+           {
+               "tag": "hy2",
+               "type": "hysteria2",
+               "server": server_ip,
+               "password": password,
+               "server_port": 443,
+               "tls": {
+                   "alpn": [
+                      "h3"
+                   ],
+                   "enabled": True,
+                   "insecure": True,
+                   "server_name": "www.google.com"
                 },
-                {
-                    "address": "https://cloudflare-dns.com/dns-query",
-                    "address_resolver": "dns-local",
-                    "detour": "direct",
-                    "strategy": "ipv4_only",
-                    "tag": "dns-direct"
-                },
-                {
-                    "address": "local",
-                    "detour": "direct",
-                    "tag": "dns-local"
-                },
-                {
-                    "address": "rcode://success",
-                    "tag": "dns-block"
-                }
-            ]
-        },
-        "inbounds": [
-            {
-                "auto_route": True,
-                "endpoint_independent_nat": True,
-                "inet4_address": "172.19.0.1/28",
-                "interface_name": "ipv4-tun",
-                "mtu": 1500,
-                "sniff": True,
-                "stack": "gvisor",
-                "strict_route": True,
-                "type": "tun"
+               "down_mbps": 60,
+               "up_mbps": 30
             }
-        ],
-        "log": {
-            "level": "warn"
-        },
-        "outbounds": [
-            {
-                "down_mbps": 100,
-                "password": password,
-                "server": server_ip,
-                "server_port": 443,
-                "tag": "proxy",
-                "tls": {
-                    "alpn": ["h3"],
-                    "enabled": True,
-                "insecure": True,
-                    "server_name": sni
-                },
-                "type": "hysteria2",
-                "up_mbps": 50
-            },
-            {
-                "tag": "direct",
-                "type": "direct"
-            },
-            {
-                "tag": "bypass",
-                "type": "direct"
-            },
-            {
-                "tag": "block",
-                "type": "block"
-            },
-            {
-                "tag": "dns-out",
-                "type": "dns"
-            }
-        ],
-        "route": {
-            "geoip": {
-                "download_url": "https://github.com/SagerNet/sing-geoip/releases/latest/download/geoip.db",
-                "download_detour": "proxy"
+       ],
+       "route": {
+           "auto_detect_interface": True,
+           "override_android_vpn": True,
+           "final": "Select",
+           "geoip": {
+               "download_url": "https://github.com/SagerNet/sing-geoip/releases/latest/download/geoip.db",
+               "download_detour": "Select"
             },
             "geosite": {
-                "download_url": "https://github.com/SagerNet/sing-geosite/releases/latest/download/geosite.db",
-                "download_detour": "proxy"
+               "download_url": "https://github.com/SagerNet/sing-geosite/releases/latest/download/geosite.db",
+               "download_detour": "Select"
             },
-            "auto_detect_interface": True,
             "rules": [
-                {
-                    "outbound": "dns-out",
-                    "port": [53]
-                },
-                {
-                    "inbound": ["dns-in"],
-                    "outbound": "dns-out"
-                },
-                 {
-                  "domain_suffix": ".ir",
-                  "geoip": [
-                     "ir",
-                     "cn",
-                     "private"
+               {
+                  "outbound": "dns-out",
+                  "port": [
+                     53
+                  ]
+               },
+               {
+                  "inbound": [
+                     "dns-in"
                   ],
-                  "outbound": "bypass"
-                  },
-                {
-                    "ip_cidr": ["224.0.0.0/3", "ff00::/8"],
-                    "outbound": "block",
-                    "source_ip_cidr": ["224.0.0.0/3", "ff00::/8"]
-                }
+                  "outbound": "dns-out"
+               },
+               {
+                  "geoip": [
+                     "ir"
+                  ],
+                  "process_name": [
+                   "WeChat"
+                 ],
+                  "outbound": "direct"
+               },
+               {
+                  "ip_cidr": [
+                     "224.0.0.0/3",
+                     "ff00::/8"
+                  ],
+                  "outbound": "block",
+                  "source_ip_cidr": [
+                     "224.0.0.0/3",
+                     "ff00::/8"
+                  ]
+               }
             ]
-        }
+         },
+       "experimental": {
+           "cache_file": {
+               "enabled": True,
+               "path": "cache.db",
+               "cache_id": "hy2",
+               "store_fakeip": True
+           }
+       },
+       "dns": {
+           "servers": [
+               {
+                   "address": "tcp://94.140.14.14",
+                   "address_resolver": "dns-local",
+                   "strategy": "prefer_ipv4",
+                   "tag": "dns-remote",
+                   "detour": "Select"
+               },
+               {
+                   "address": "94.140.14.14",
+                   "detour": "direct",
+                   "tag": "dns-local"
+               },
+               {
+                   "address": "rcode://success",
+                   "tag": "dns-block"
+               }
+           ],
+           "rules": [
+               {
+                   "domain": [
+                      "9.9.9.9",
+                      "frankfurt.amin2plus.store",
+                      "estonia.amin2plus.store"
+                   ],
+                   "server": "dns-local"
+               },
+               {
+                   "outbound": "direct",
+                   "server": "dns-local"
+               }
+           ],
+           "final": "dns-remote",
+           "reverse_mapping": True,
+           "strategy": "prefer_ipv4",
+           "independent_cache": True
+       },
+       "inbounds": [
+          {
+             "auto_route": True,
+             "endpoint_independent_nat": True,
+             "inet4_address": "172.19.0.1/28",
+             "interface_name": "ipv4-tun",
+             "mtu": 1500,
+             "sniff": True,
+             "stack": "gvisor",
+             "strict_route": True,
+             "type": "tun"
+          }
+       ],
+       "log": {
+          "level": "warn"
+       }
     }
 
     # Write the JSON to a file
@@ -150,5 +191,5 @@ def generate_json(input_link):
     print(f"Output JSON file '{output_filename}' created successfully.")
 
 # for usage please put your link down here
-input_link = "hy2://b2cac3aa0d7f6f6a@94.131.15.131:443?insecure=1&sni=www.google.com#dariush-ee"
+input_link = "hy2://2b6010bb11e6571f@85.215.50.93:443?insecure=1&sni=www.google.com#hosein-fk"
 generate_json(input_link)
